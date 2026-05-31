7642d240b77576af134c091f7ad3d570c6e0fe7c
shaileshljadhav <shaileshjadhav403@gmail.com>
2026-05-31 15:32:06 +0530

    Rout configuration to load mfe - lazy loading

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -1,3 +1,27 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Redirect empty path to /product
+  { path: '', redirectTo: 'product', pathMatch: 'full' }, 
+  
+  // Standard routes
+  { 
+    path: 'product', 
+    loadComponent: () =>
+        loadRemoteModule({
+            type: 'module',
+            remoteEntry: 'http://localhost:4201/remoteEntry.js',
+            exposedModule: './Component',
+        }).then((m) => m.AppComponent),
+  },
+  { 
+    path: 'account', 
+    loadComponent: () =>
+        loadRemoteModule({
+            type: 'module',
+            remoteEntry: 'http://localhost:4202/remoteEntry.js',
+            exposedModule: './Component',
+        }).then((m) => m.AppComponent),
+  },
+];
